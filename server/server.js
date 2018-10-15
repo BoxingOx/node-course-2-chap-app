beb9b59e18db0076fc2dc86b9d1a7971b0d53612
Sean Dingemans <sxd616@cs.bham.ac.uk>
2018-10-15 13:39:20 +0100

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,20 +15,39 @@
   io.on('connection', (socket) =>{ // indiv socket as opposed to all of the users server notices that a client has connected. Perspective of server
     console.log('New user connected');
 
-    socket.on('createMessage',(message) =>{
-      console.log('createMessage', message);
-      io.emit('newMessage', {
-        from: message.from,
-        text: message.text,
+
+
+      socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+    })
+
+      socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
         createdAt: new Date().getTime()
       })
-    });
+
+      socket.on('createMessage',(message) =>{
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+          from: message.from,
+          text: message.text,
+          createdAt: new Date().getTime()
+        })// end inner io.emit
+      // socket.broadcast.emit('newMessage',{
+      //   from: message.from,
+      //   text: message.text,
+      //   createdAt: new Date().getTime()
+      // })  // broadcast to everyone but yourself
+    }); // end outer soket.on for createMessage
 
     socket.on('disconnect', () =>{
       console.log('User was disconnected');
     });
 
-  });
+  });// end outer io.on
 
   server.listen(port, () =>{
     console.log(`Server is up on ${port}`)
