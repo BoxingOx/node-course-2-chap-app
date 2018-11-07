9d016d9e8e8d7ea52ca1daee0629a3f72133e7de
Sean Dingemans <sxd616@cs.bham.ac.uk>
2018-11-07 18:51:21 +0000

    Added Mustachejs for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,20 +9,36 @@ var socket = io(); //client method from perspective of client
   });// end disconnect listener
 
   socket.on('newMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);  // safer to inject li as a var as opposed to appending template String directly
-    jQuery('#messages').append(li);// append to existing list i think
+      var formattedTime = moment(message.createdAt).format('h:mm a');
+      var template = jQuery('#message-template').html();  // html returns the markup inside message-temlatte
+      var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+      }); // actually renders the stuff inside of the template variable. It could be many things
+
+    jQuery('#messages').append(html);
+
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);  // safer to inject li as a var as opposed to appending template String directly
+    // jQuery('#messages').append(li);// append to existing list i think
   });// end newMessage Listener
 
   socket.on('newLocationMessage', function(message){
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a); // append the link
-    jQuery('#messages').append(li);
+      var formattedTime = moment(message.createdAt).format('h:mm a');
+      var template = jQuery('#location-message-template').html();
+      var html = Mustache.render(template, {
+      from: message.from,
+      url: message.url,
+      createdAt: formattedTime
+      }); // end Mustache.render
+    jQuery('#messages').append(html);
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">My current location</a>');
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href', message.url);
+    // li.append(a); // append the link
+    // jQuery('#messages').append(li);
   }); // end newLocationMessage listener
 
   jQuery('#message-form').on('submit', function(e){ // event name and response fxn( with e arg)
